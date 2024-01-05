f5fbf56a243b0492831a2cf205b6bb3152a5a945
pinlunix <pheonlu@gmail.com>
2024-01-05 02:45:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-portfolio">
+    <Router >
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
